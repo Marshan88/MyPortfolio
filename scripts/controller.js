3f2013712106a0fe6bbda1cb887f415cf26d1565
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-03-31 11:09:27 +0200

    Minor CSS
    Cards and about me button

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,3 +1,16 @@
+// var firstsec = document.getElementById('main');
+// var secondsec = document.getElementById('sub-section-alternative');
+// var lastScrollTop = 0;
+// window.onscroll = function () {
+//   var st = window.pageYOffset || document.documentElement.scrollTop;
+//   if (st > lastScrollTop) {
+//     secondsec.scrollIntoView({ behavior: "smooth" });
+//   } else {
+//     firstsec.scrollIntoView({ behavior: "smooth" });
+//   }
+//   lastScrollTop = st <= 0 ? 0 : st;
+// }
+
 function toggleButton() {
   const navList = document.getElementById("navList");
   navList.classList.toggle("show");
